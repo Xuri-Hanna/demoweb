178cd832f71db300ae7c2ac5fff8373de892dd23
NQTHLR <126398372+NQTHLR@users.noreply.github.com>
2024-05-15 01:23:30 +0700

    Update index.php

--- quan_tri/index.php
+++ quan_tri/index.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
 	$bien_bao_mat="co";
-    include("../ket_noi.php");	
+    // include("../ket_noi.php");	
 	include("chuc_nang/quan_tri_2/xac_dinh_dang_nhap.php");
-	include("chuc_nang/quan_tri_2/ham.php");
+	// include("chuc_nang/quan_tri_2/ham.php");
 	if(isset($xac_dinh_dang_nhap))
 	{
 		if($xac_dinh_dang_nhap=="co")
