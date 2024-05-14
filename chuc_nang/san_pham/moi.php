fa3c070981c326a1f746b87f13b0a6ce4e9a537c
NQTHLR <126398372+NQTHLR@users.noreply.github.com>
2024-05-15 01:50:49 +0700

    Update moi.php

--- chuc_nang/san_pham/moi.php
+++ chuc_nang/san_pham/moi.php
@@ -1,8 +1,9 @@
 <br />
 <center>
 	<?php 
+	   	$conn = new mysqli("xoaserver.mysql.database.azure.com", "sqladmin", "#Nqthlr123", "ban_hang");
 		$tv="SELECT id, ten, hinh_anh FROM san_pham ORDER BY id DESC LIMIT 0,3";
-		$tv_1=mysqli_query($connection, $tv); // Thay $connection bằng biến kết nối cơ sở dữ liệu của bạn
+		$tv_1=mysqli_query($conn, $tv); // Thay $connection bằng biến kết nối cơ sở dữ liệu của bạn
 		while($tv_2=mysqli_fetch_array($tv_1))
 		{
 			$link_anh="hinh_anh/san_pham/".$tv_2['hinh_anh'];
